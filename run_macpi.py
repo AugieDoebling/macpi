6c6d74cb7aa98398fc8530c4bfbfc60d1a2e41e1
Augie Doebling <augustdoebling@gmail.com>
2022-09-20 19:07:48 -0700

    remove unnecessary lib files

--- run_macpi.py
+++ run_macpi.py
@@ -2,13 +2,14 @@
 # -*- coding:utf-8 -*-
 import sys
 import os
-picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
-libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
-if os.path.exists(libdir):
-    sys.path.append(libdir)
+# picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+# libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+# if os.path.exists(libdir):
+#     sys.path.append(libdir)
 
 import logging
-from waveshare_epd import epd7in5_V2
+# from lib/waveshare_epd import epd7in5_V2
+import epd7in5_V2
 import time
 from PIL import Image,ImageDraw,ImageFont
 import traceback
